d5787091491c968b32e0a8cf3c4024bffad83aee
Prashanth Venkat Gumma <pragumma0@publicisgroupe.net>
2018-08-17 11:16:14 +0530

    removed unused imports

--- src/main/java/com/vadrin/apodwallpaper/services/ImageService.java
+++ src/main/java/com/vadrin/apodwallpaper/services/ImageService.java
@@ -7,7 +7,6 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
-import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Arrays;
